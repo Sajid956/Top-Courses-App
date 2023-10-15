022961bc9107881b65d42f6aae0263d81d5b1203
Sajid956 <sajidhussai7863@gmail.com>
2023-10-15 19:25:40 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { CardContainer } from './components/CardContainer';
+import { Header } from './components/Header';
+import { Navbar } from './components/Navbar';
+import { filterData, apiUrl } from './data';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+
+  const [reqData, setReqData] = useState(filterData[0]);
+  function updateData(data) {
+    setReqData(data);
+  }
+
+  // console.log(filterData);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='min-h-screen flex flex-col bg-bg-dark2'>
+      <ToastContainer/>
+      <Header></Header>
+      <Navbar title = {filterData} getCourses = {(data) =>updateData(data)} reqData = {reqData}/>
+      <CardContainer url = {apiUrl} reqData = {reqData}/>
+   </div>
   );
 }
 
